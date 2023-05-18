508e4e3330909eb8489d0e3dc0fb689ac3c98b56
salman8896 <salmanshahid8896@gmail.com>
2023-05-19 00:12:03 +0500

    Login Screen Responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import Login from "./components/LoginScreen/Login";
 import Notes from "./components/Notes/Notes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    sessionStorage.setItem(
+      "login_account_token",
+      JSON.stringify({
+        email: "admin@admin.com",
+        password: "admin",
+      })
+    );
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
--- src/components/LoginScreen/Login.tsx
+++ src/components/LoginScreen/Login.tsx
@@ -3,25 +3,40 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const Login = () => {
-  const navigate = useNavigate();
+  const Navigate = useNavigate();
   const [Email, setEmail] = useState<string>("");
   const [Password, setPassword] = useState<string>("");
   const [EmailError, setEmailError] = useState<string | string[]>("");
   const [PasswordError, setPasswordError] = useState<string | string[]>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const ValidEmailAddress = "admin@admin.com";
+  const ValidPassword = "admin";
 
   const handleSubmit = () => {
-    navigate("/home");
-    if (Email === "") {
-      setEmailError("Email is required");
-    }
-    if (Password === "") {
-      setPasswordError("Password is required");
+    setLoading(true);
+    if (
+      Email === "" ||
+      !Email.match(ValidEmailAddress) ||
+      Password === "" ||
+      !Password.match(ValidPassword)
+    ) {
+      setLoading(false);
+      setEmailError(["Email address is required!", "Wrong email address!"]);
+      setPasswordError(["Password is required!", "Wrong password!"]);
+    } else {
+      const login_body_data = {
+        email: Email,
+        password: Password,
+      };
+      console.log(login_body_data);
+      Navigate("/home");
     }
   };
 
   return (
     <div className="min-h-screen min-w-full bg-[#FAFAFA] flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center bg-white shadow-md min-h-[40%] min-w-[35%] rounded-lg py-5">
+      <div className="flex flex-col justify-center items-center bg-white shadow-md lg:min-h-[40%] lg:min-w-[35%] rounded-lg py-5 min-h-[50vh] min-w-[60vw] lg:px-0 px-5">
         <h3 className="text-[#1A3A5F] font-bold font-mulish text-2xl leading-6 my-5">
           Welcome Back!
         </h3>
@@ -38,9 +53,21 @@ const Login = () => {
               setEmail(e.target.value);
             }}
           />
-          <label className="label">
-            <span className="label-text-alt text-red-300">{EmailError}</span>
-          </label>
+          {Email === "" && loading ? (
+            <label className="label">
+              <span className="label-text-alt text-red-300">
+                {EmailError[0]}
+              </span>
+            </label>
+          ) : (
+            !Email.match(ValidEmailAddress) && (
+              <label className="label">
+                <span className="label-text-alt text-red-300">
+                  {EmailError[1]}
+                </span>
+              </label>
+            )
+          )}
         </div>
         <div className="form-control w-full max-w-xs my-4">
           <label className="label">
@@ -55,15 +82,28 @@ const Login = () => {
               setPassword(e.target.value);
             }}
           />
-          <label className="label">
-            <span className="label-text-alt text-red-300">{PasswordError}</span>
-          </label>
+          {Password === "" && loading ? (
+            <label className="label">
+              <span className="label-text-alt text-red-300">
+                {PasswordError[0]}
+              </span>
+            </label>
+          ) : (
+            !Password.match(ValidPassword) && (
+              <label className="label">
+                <span className="label-text-alt text-red-300">
+                  {PasswordError[1]}
+                </span>
+              </label>
+            )
+          )}
         </div>
         <Button
           variant={"primary"}
           size={"lg"}
           className="w-[40%] m-5 px-5"
           onClick={handleSubmit}
+          isLoading={loading}
         >
           Login
         </Button>
